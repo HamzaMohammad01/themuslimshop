c5b2fe3c5e830709a6130f5fbf2bee2421007ecb
Hamza Mohammad <mohd2010hamza@gmail.com>
2024-05-24 20:42:31 +0530

    Removed ignored files from version control

--- src/api/client.js
+++
@@ -1,7 +0,0 @@
-import { create } from "apisauce";
-
-const apiClient = create({
-	baseURL: "http://localhost:8000/api/",
-});
-
-export default apiClient;
--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-	return (
-		<div className="h-[90svh] w-svw flex flex-col items-center justify-center">
-			<div className="h-48 w-48 rounded-full border-primary border-x-8 animate-spin" />
-		</div>
-	);
-}
